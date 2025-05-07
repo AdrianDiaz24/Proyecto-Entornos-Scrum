f4e05d06232e32ecd845c01e9a233e68a689f3df
GunterMagno <bcalejandro@iesrafaelalberti.es>
2025-05-07 17:22:42 +0200

    Inicio con tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.4")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.4")
+    testImplementation("io.kotest:kotest-property:5.5.4")
 }
 
 tasks.test {
