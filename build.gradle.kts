0cfb940a51bc057559f7157b58e4972704234124
falbmun0906 <amfrancisco@iesrafaelalberti.es>
2025-05-21 00:02:49 +0200

    "Creo rama Linting"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 group = "es.prog2425.taskmanager"
